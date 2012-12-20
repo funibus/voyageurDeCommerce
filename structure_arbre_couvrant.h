90c209c911331573db0d5ef1c0967719e9cb91b7
Noneangel <connexion.pierremacherel@gmail.com>
2012-12-20 19:28:08 +0100

    correction sintaxe, free arbre, iterisation liberer...

--- structure_arbre_couvrant.h
+++ structure_arbre_couvrant.h
@@ -11,10 +11,9 @@ typedef struct element_liste* element_liste;
 
 
 element_liste* creer_arbre (int nombre_villes);
-void inserer_element_arbre (element_liste* arbre, int sommet1, int sommet2);
+void inserer_element_arbre (element_liste arbre[], int sommet1, int sommet2);
 void liberer_liste (element_liste element);
-void liberer_arbre (element_liste* arbre, int nombre_villes);
-void parcourir_arbre_aux (FILE* fichier, element_liste* arbre, int sommet, int pere, int b);
+void liberer_arbre (element_liste arbre[], int nombre_villes);
 void parcourir_arbre (FILE* fichier, element_liste* arbre);
 double distance_parcourue (FILE* fichier, Matrice matrice_poids);
 
